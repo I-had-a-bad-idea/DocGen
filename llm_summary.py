6515d7cbcb0f55cd3d3aa4d97a3ff7f9b803b7fc
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2025-12-28 18:51:24 +0100

    Maybe better

--- llm_summary.py
+++ llm_summary.py
@@ -3,12 +3,15 @@ from ollama import AsyncClient
 MODEL = "deepseek-r1:1.5b"
 
 BASE_PROMPT = """
-In the given markdown replace all code blocks with an summary of them.
-Summary should include a high-level and low-level explaination.
+In the given markdown REPLACE all code blocks with an SUMMARY of them.
+The summary should include a DETAILED high-level and an even more DETAILED low-level explanation.
+Explain everything in the code (variables, functions, etc.), but dont add hallucinate anything new.
+Use Markdown to make it look good.
 Keep the style.
 Keep the rest as it is.
+Make it detailed.
+Dont use markdown blocks (```markdown ```)
 Dont write stuff like "here is..."
-Dont use markdown blocks, like ```markdown ```
 """
 
 
