794406fb4e5a63b8bb793139d9084b9586bed044
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2025-12-28 15:48:51 +0100

    Generate md

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from code_structure_extractor import code_structure_of_file, print_symbols
 import sys
-
+from markdown_generator import generate_markdown
 
 def main():
     if len(sys.argv) >= 2:
         file_path = sys.argv[1]
         symbols = code_structure_of_file(file_path)
         print_symbols(symbols)
+        generate_markdown(file_path, symbols)
 
 
 if __name__ == "__main__":
