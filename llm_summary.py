898aac3589c511f07e2eb1d968ecab5d30c63428
I had a bad idea <182666156+I-had-a-bad-idea@users.noreply.github.com>
2025-12-28 17:13:55 +0100

    Simpler llm

--- llm_summary.py
+++ llm_summary.py
@@ -1,21 +1,18 @@
 from ollama import AsyncClient
 
-MODEL = "deepseek-coder:6.7b"
+MODEL = "deepseek-r1:1.5b"
 
 BASE_PROMPT = """
-You are a documentation engine.
-Given the markdown below, replace the code with a summary that:
-1. Explains what the code does at a high level
-2. Explains its purpose and behavior in plain language
-3. Highlights any important details, like side effects or constants
-4. Uses a clean, human-readable style
-5. Dont use markdonw blocks (```markdown  ```)!!"""
+In the given markdown replace all code blocks with an summary of them.
+Summary should include a high-level and low-level explaination.
+Keep the rest as it is.
+"""
+
 
 ollama = AsyncClient()
 
 
 async def summarize_code_in_markdown(markdown: str) -> str:
     prompt = BASE_PROMPT + "\n\n" + markdown
-    print("Generating LLLM summaries")
     resp = await ollama.generate(MODEL, prompt)
     return resp.response
